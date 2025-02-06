2e58b67831cd4715fc817c39bf80647fd92288a6
aishwarya <ashp17star@gmail.com>
2025-02-06 12:00:16 +0530

    Initial commit

---
+++ app/src/androidTest/java/com/bitcode/a30_12_24_webservices_demo_version1/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.bitcode.a30_12_24_webservices_demo_version1
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.bitcode.a30_12_24_webservices_demo_version1", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/bitcode/a30_12_24_webservices_demo_version1/Post.kt
@@ -0,0 +1,10 @@
+package com.bitcode.a30_12_24_webservices_demo_version1
+
+import java.io.Serializable
+
+data class Post(
+    var userId : Int,
+    var id : Int,
+    var title : String,
+    var body : String
+)
